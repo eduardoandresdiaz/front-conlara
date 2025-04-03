0c853c4cbcd9556c22ad03bfc3fe65b69ff15185
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-02 23:59:38 -0300

    vamos dale

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import{BrowserRouter} from 'react-router-dom'
-import { UserProvider } from './context/UserContext.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserProvider } from './context/UserContext.jsx';
+import DetallesProducto from './DetallesProducto'; // Importar el componente DetallesProducto
+import App from './App.jsx'; // Componente principal
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-       <App />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/productos/:id" element={<DetallesProducto />} />
+        </Routes>
       </UserProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
