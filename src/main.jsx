9f5ec9a16a7123554e228f8a5b8f0387902bd4b3
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-13 20:56:41 -0300

    modificado main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { UserProvider } from './context/UserContext.jsx';
-import DetallesProducto from './views/Detalles/Detalles.jsx'; // Ruta correcta al componente
+import DetallesProducto from './views/Detalles/Detalles.jsx'; // Componente de detalles
 import App from './App.jsx'; // Componente principal
 import { HelmetProvider } from 'react-helmet-async';
 
@@ -15,9 +15,34 @@ createRoot(document.getElementById('root')).render(
           <Routes>
             <Route path="*" element={<App />} />
             <Route path="/productos/:id" element={<DetallesProducto />} />
+            <Route path="/productos/share/:id" element={<DetallesProducto />} />
           </Routes>
         </HelmetProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
 );
+
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { UserProvider } from './context/UserContext.jsx';
+// import DetallesProducto from './views/Detalles/Detalles.jsx'; // Ruta correcta al componente
+// import App from './App.jsx'; // Componente principal
+// import { HelmetProvider } from 'react-helmet-async';
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <UserProvider>
+//         <HelmetProvider>
+//           <Routes>
+//             <Route path="*" element={<App />} />
+//             <Route path="/productos/:id" element={<DetallesProducto />} />
+//           </Routes>
+//         </HelmetProvider>
+//       </UserProvider>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
