4a68f27d7fa6d9911e02eb75a67546a4415096e0
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2025-04-13 20:39:44 -0300

    modificado vite.config

--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,32 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      '/products/share': {
+        target: 'https://ecommerce-9558.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/products\/share/, '/products/share'),
+      },
     },
   },
 });
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-// })
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://ecommerce-9558.onrender.com',
+//         changeOrigin: true,
+//         secure: false,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// });
+
+
