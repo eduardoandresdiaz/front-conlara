897dc50ae1469b3fda7d5ce56884b0daaf6d6e96
eduardoandresdiaz <eduardoandresdiaz@gmail.com>
2026-05-26 23:13:35 -0300

    buscador

--- src/views/MisProductos/MisProductos.jsx
+++ src/views/MisProductos/MisProductos.jsx
@@ -10,6 +10,7 @@ const MisProductos = () => {
   const [mensajeStock, setMensajeStock] = useState('');
   const [filtroAgotados, setFiltroAgotados] = useState(false);
   const [filtroVencidos, setFiltroVencidos] = useState(false);
+  const [busqueda, setBusqueda] = useState(''); // NUEVO estado buscador
   const { user } = useUser();
   const navigate = useNavigate();
 
@@ -19,7 +20,9 @@ const MisProductos = () => {
         throw new Error("No se encontró el email del usuario.");
       }
 
-      const response = await fetch(`https://ecommerce-9558.onrender.com/products/by-creator?creatorEmail=${user.email}`);
+      const response = await fetch(
+        `https://ecommerce-9558.onrender.com/products/by-creator?creatorEmail=${user.email}`
+      );
       if (!response.ok) {
         throw new Error('Error al obtener los productos del usuario');
       }
@@ -42,32 +45,40 @@ const MisProductos = () => {
       const imgUrl = producto.imgUrl;
       const publicId = imgUrl.split('/').pop().split('.')[0];
 
-      const responseImg = await fetch(`https://ecommerce-9558.onrender.com/file-upload/deleteImage/${publicId}`, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
+      const responseImg = await fetch(
+        `https://ecommerce-9558.onrender.com/file-upload/deleteImage/${publicId}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       if (!responseImg.ok) {
         throw new Error('Error al eliminar la imagen asociada al producto');
       }
 
-      const responseProduct = await fetch(`https://ecommerce-9558.onrender.com/products/${producto.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
+      const responseProduct = await fetch(
+        `https://ecommerce-9558.onrender.com/products/${producto.id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       if (!responseProduct.ok) {
         throw new Error('Error al eliminar el producto');
       }
 
       await responseProduct.json();
-      setProductos((prevProductos) => prevProductos.filter((p) => p.id !== producto.id));
+      setProductos((prevProductos) =>
+        prevProductos.filter((p) => p.id !== producto.id)
+      );
     } catch (error) {
       console.error('Error al intentar eliminar el producto y la imagen:', error.message);
     }
@@ -90,14 +101,17 @@ const MisProductos = () => {
       const nuevoStock = producto.stock - 1;
 
       const token = localStorage.getItem('token');
-      const response = await fetch(`https://ecommerce-9558.onrender.com/products/${producto.id}`, {
-        method: 'PATCH',
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ stock: nuevoStock }),
-      });
+      const response = await fetch(
+        `https://ecommerce-9558.onrender.com/products/${producto.id}`,
+        {
+          method: 'PATCH',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ stock: nuevoStock }),
+        }
+      );
 
       if (!response.ok) {
         throw new Error('Error al actualizar el stock');
@@ -120,48 +134,52 @@ const MisProductos = () => {
     fetchProductos();
   }, []);
 
-  // Filtrado dinámico
+  // Filtrado dinámico con buscador
   const productosFiltrados = productos.filter((p) => {
     const esAgotado = p.stock === 0;
     const esVencido = new Date(p.expiresAt) < new Date();
-  
-    // Si ambos filtros están activos → mostrar productos que cumplan al menos uno
-    if (filtroAgotados && filtroVencidos) {
-      return esAgotado || esVencido;
-    }
-  
-    if (filtroAgotados) return esAgotado;
-    if (filtroVencidos) return esVencido;
-  
-    return true;
+    const coincideBusqueda =
+      p.name.toLowerCase().includes(busqueda.toLowerCase()) ||
+      p.category.name.toLowerCase().includes(busqueda.toLowerCase());
+
+    if (filtroAgotados && filtroVencidos) return (esAgotado || esVencido) && coincideBusqueda;
+    if (filtroAgotados) return esAgotado && coincideBusqueda;
+    if (filtroVencidos) return esVencido && coincideBusqueda;
+    return coincideBusqueda;
   });
+
   const cantidadAgotados = productos.filter((p) => p.stock === 0).length;
   const cantidadVencidos = productos.filter((p) => new Date(p.expiresAt) < new Date()).length;
-    
-  
-  
-  
 
   return (
     <div className="listadoProductos">
-     <h1 className="tituloSuperior">Mis Publicaciones</h1>
+      <h1 className="tituloSuperior">Mis Publicaciones</h1>
 
-     <div className="filtrosContainer">
-  <button
-    className="filtroBoton"
-    onClick={() => setFiltroAgotados(!filtroAgotados)}
-  >
-    {filtroAgotados ? "Mostrar Todo" : `Productos Agotados (${cantidadAgotados})`}
-  </button>
-  <button
-    className="filtroBoton"
-    onClick={() => setFiltroVencidos(!filtroVencidos)}
-  >
-    {filtroVencidos ? "Mostrar Todo" : `Ofertas Vencidas (${cantidadVencidos})`}
-  </button>
-</div>
+      {/* Buscador */}
+      <form className="buscadorContainer" onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Buscar mis productos..."
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+          className="buscadorInput"
+        />
+      </form>
 
-
+      <div className="filtrosContainer">
+        <button
+          className="filtroBoton"
+          onClick={() => setFiltroAgotados(!filtroAgotados)}
+        >
+          {filtroAgotados ? "Mostrar Todo" : `Productos Agotados (${cantidadAgotados})`}
+        </button>
+        <button
+          className="filtroBoton"
+          onClick={() => setFiltroVencidos(!filtroVencidos)}
+        >
+          {filtroVencidos ? "Mostrar Todo" : `Ofertas Vencidas (${cantidadVencidos})`}
+        </button>
+      </div>
 
       {error && <p className="listadoProductos__error">{error}</p>}
 
@@ -175,11 +193,10 @@ const MisProductos = () => {
               <p><strong>Precio:</strong> ${producto.price}</p>
               <p><strong>Stock:</strong> {producto.stock}</p>
               <div className="detallesCompactos">
-              <p><strong>Categoría:</strong> {producto.category.name}</p>
-              <p><strong>Fecha de Creación:</strong> {new Date(producto.createdAt).toLocaleString()}</p>
-              <p><strong>Fecha de Expiración:</strong> {new Date(producto.expiresAt).toLocaleDateString()}</p>
-</div>
-
+                <p><strong>Categoría:</strong> {producto.category.name}</p>
+                <p><strong>Fecha de Creación:</strong> {new Date(producto.createdAt).toLocaleString()}</p>
+                <p><strong>Fecha de Expiración:</strong> {new Date(producto.expiresAt).toLocaleDateString()}</p>
+              </div>
 
               <button
                 className="vendidoBoton"
@@ -234,6 +251,243 @@ const MisProductos = () => {
 };
 
 export default MisProductos;
+
+// import { useEffect, useState } from 'react';
+// import { useUser } from "../../context/UserContext";
+// import { useNavigate } from 'react-router-dom';
+// import './MisProductos.css';
+
+// const MisProductos = () => {
+//   const [productos, setProductos] = useState([]);
+//   const [error, setError] = useState('');
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const [mensajeStock, setMensajeStock] = useState('');
+//   const [filtroAgotados, setFiltroAgotados] = useState(false);
+//   const [filtroVencidos, setFiltroVencidos] = useState(false);
+//   const { user } = useUser();
+//   const navigate = useNavigate();
+
+//   const fetchProductos = async () => {
+//     try {
+//       if (!user || !user.email) {
+//         throw new Error("No se encontró el email del usuario.");
+//       }
+
+//       const response = await fetch(`https://ecommerce-9558.onrender.com/products/by-creator?creatorEmail=${user.email}`);
+//       if (!response.ok) {
+//         throw new Error('Error al obtener los productos del usuario');
+//       }
+//       const data = await response.json();
+//       setProductos(data);
+//       setError('');
+//     } catch (error) {
+//       setError(error.message);
+//       setProductos([]);
+//     }
+//   };
+
+//   const eliminarProducto = async (producto) => {
+//     try {
+//       const token = localStorage.getItem('token');
+//       if (!token) {
+//         throw new Error("No se encontró un token de autenticación.");
+//       }
+
+//       const imgUrl = producto.imgUrl;
+//       const publicId = imgUrl.split('/').pop().split('.')[0];
+
+//       const responseImg = await fetch(`https://ecommerce-9558.onrender.com/file-upload/deleteImage/${publicId}`, {
+//         method: 'DELETE',
+//         headers: {
+//           'Authorization': `Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//       });
+
+//       if (!responseImg.ok) {
+//         throw new Error('Error al eliminar la imagen asociada al producto');
+//       }
+
+//       const responseProduct = await fetch(`https://ecommerce-9558.onrender.com/products/${producto.id}`, {
+//         method: 'DELETE',
+//         headers: {
+//           'Authorization': `Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//       });
+
+//       if (!responseProduct.ok) {
+//         throw new Error('Error al eliminar el producto');
+//       }
+
+//       await responseProduct.json();
+//       setProductos((prevProductos) => prevProductos.filter((p) => p.id !== producto.id));
+//     } catch (error) {
+//       console.error('Error al intentar eliminar el producto y la imagen:', error.message);
+//     }
+//   };
+
+//   const marcarVendido = async (producto) => {
+//     try {
+//       if (producto.stock <= 0) {
+//         setMensajeStock("No quedan productos en stock");
+//         setModalOpen(true);
+//         return;
+//       }
+
+//       if (new Date(producto.expiresAt) < new Date()) {
+//         setMensajeStock("La oferta ya venció");
+//         setModalOpen(true);
+//         return;
+//       }
+
+//       const nuevoStock = producto.stock - 1;
+
+//       const token = localStorage.getItem('token');
+//       const response = await fetch(`https://ecommerce-9558.onrender.com/products/${producto.id}`, {
+//         method: 'PATCH',
+//         headers: {
+//           'Authorization': `Bearer ${token}`,
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ stock: nuevoStock }),
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Error al actualizar el stock');
+//       }
+
+//       setProductos((prevProductos) =>
+//         prevProductos.map((p) =>
+//           p.id === producto.id ? { ...p, stock: nuevoStock } : p
+//         )
+//       );
+
+//       setMensajeStock(`Te quedan ${nuevoStock} productos en stock`);
+//       setModalOpen(true);
+//     } catch (error) {
+//       console.error('Error al marcar como vendido:', error.message);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchProductos();
+//   }, []);
+
+//   // Filtrado dinámico
+//   const productosFiltrados = productos.filter((p) => {
+//     const esAgotado = p.stock === 0;
+//     const esVencido = new Date(p.expiresAt) < new Date();
+  
+//     // Si ambos filtros están activos → mostrar productos que cumplan al menos uno
+//     if (filtroAgotados && filtroVencidos) {
+//       return esAgotado || esVencido;
+//     }
+  
+//     if (filtroAgotados) return esAgotado;
+//     if (filtroVencidos) return esVencido;
+  
+//     return true;
+//   });
+//   const cantidadAgotados = productos.filter((p) => p.stock === 0).length;
+//   const cantidadVencidos = productos.filter((p) => new Date(p.expiresAt) < new Date()).length;
+    
+  
+  
+  
+
+//   return (
+//     <div className="listadoProductos">
+//      <h1 className="tituloSuperior">Mis Publicaciones</h1>
+
+//      <div className="filtrosContainer">
+//   <button
+//     className="filtroBoton"
+//     onClick={() => setFiltroAgotados(!filtroAgotados)}
+//   >
+//     {filtroAgotados ? "Mostrar Todo" : `Productos Agotados (${cantidadAgotados})`}
+//   </button>
+//   <button
+//     className="filtroBoton"
+//     onClick={() => setFiltroVencidos(!filtroVencidos)}
+//   >
+//     {filtroVencidos ? "Mostrar Todo" : `Ofertas Vencidas (${cantidadVencidos})`}
+//   </button>
+// </div>
+
+
+
+//       {error && <p className="listadoProductos__error">{error}</p>}
+
+//       <div className="listadoProductos__list">
+//         {productosFiltrados.length === 0 && !error ? (
+//           <p>No hay productos para mostrar.</p>
+//         ) : (
+//           productosFiltrados.map((producto) => (
+//             <div key={producto.id} className="listadoProductos__details">
+//               <h2>{producto.name}</h2>
+//               <p><strong>Precio:</strong> ${producto.price}</p>
+//               <p><strong>Stock:</strong> {producto.stock}</p>
+//               <div className="detallesCompactos">
+//               <p><strong>Categoría:</strong> {producto.category.name}</p>
+//               <p><strong>Fecha de Creación:</strong> {new Date(producto.createdAt).toLocaleString()}</p>
+//               <p><strong>Fecha de Expiración:</strong> {new Date(producto.expiresAt).toLocaleDateString()}</p>
+// </div>
+
+
+//               <button
+//                 className="vendidoBoton"
+//                 onClick={() => marcarVendido(producto)}
+//                 disabled={producto.stock === 0 || new Date(producto.expiresAt) < new Date()}
+//               >
+//                 Vendido
+//               </button>
+
+//               <div className="imagenContainer">
+//                 <img
+//                   src={producto.imgUrl}
+//                   alt={`Imagen de ${producto.name}`}
+//                   className="productoImagen"
+//                 />
+//                 {producto.stock === 0 && (
+//                   <div className="agotadoOverlay">AGOTADO</div>
+//                 )}
+//                 {new Date(producto.expiresAt) < new Date() && producto.stock > 0 && (
+//                   <div className="ofertaVencidaOverlay">OFERTA VENCIDA</div>
+//                 )}
+//               </div>
+
+//               <button
+//                 className="eliminarBoton"
+//                 onClick={() => eliminarProducto(producto)}
+//               >
+//                 Eliminar Publicación
+//               </button>
+
+//               <button
+//                 className="modificarBoton"
+//                 onClick={() => navigate(`/ModifyProduct/${producto.id}`)}
+//               >
+//                 Modificar Publicacion
+//               </button>
+//             </div>
+//           ))
+//         )}
+//       </div>
+
+//       {modalOpen && (
+//         <div className="modalOverlay">
+//           <div className="modalContent">
+//             <p>{mensajeStock}</p>
+//             <button onClick={() => setModalOpen(false)}>Entendido</button>
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MisProductos;
 
 
 // import { useEffect, useState } from 'react';
